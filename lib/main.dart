eb985d321285cff55b0452cc8fe781a8d010ac04
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2020-03-09 22:58:17 -0300

    fix all repositories

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          child: FlareActor("assets/AnimGears.flr", animation: "Untitled",),
+        ),
+      ),
+    );
+  }
+}
